1deb32f65212a077bca13102bc2fa661fe0f094c
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-08-28 22:56:00 +0530

    addeed body parser middleware

--- server/flightsAndSearch/src/index.js
+++ server/flightsAndSearch/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
 const setupServer = async () => {
     
+    //Create express object
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     });
