451a19c352fee273d46652c2a01eeeca35e537c7
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-08-29 19:15:43 +0530

    added city repository and service

---
+++ server/flightsAndSearch/src/repository/city-repository.js
@@ -0,0 +1,57 @@
+const {City} = require('../models/index');
+
+class CityRepository {
+
+    async createCity({name}){
+        try {
+            const city = await City.create({
+                name
+            });
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong in repository layer")
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong in repository layer")
+            throw {error};
+        } 
+    }
+
+    async updateCity(cityId, data) {
+        try{
+            const city = await City.update(data,{
+                where:{
+                    id:cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong in repository layer")
+            throw {error};
+        } 
+    }
+    
+    async deleteCity(cityId){
+        try {
+            await City.destroy({
+                where: {
+                    id: cityId
+                }
+            });
+            return true;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong in repository layer");
+            throw {error};
+        }
+    }
+
+}
+
+module.exports = CityRepository;
---
+++ server/flightsAndSearch/src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require("../repository/index");
+
+class CityService {
+    constructor() {
+        this.CityRepository = new CityRepository();
+    }
+
+    async createCity(data) {
+        try {
+            const city = await this.CityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong at service layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await this.CityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong at service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId,data) {
+        try {
+            const city = await this.CityRepository.updateCity(cityId,data);
+            return city;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong at service layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const response = await this.CityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            console.log("FlightsAndSearch: Something went wrong at service layer");
+            throw {error};
+        }
+    }
+}
