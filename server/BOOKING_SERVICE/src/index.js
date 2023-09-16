73483df064cfbad45432359aa92150a34a793e0c
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-09-16 21:20:12 +0530

    basic setup

---
+++ server/BOOKING_SERVICE/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require('./routes/index');
+
+const setupServer = async() => {
+
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    
+    app.use('/api', apiRoutes);
+    
+    app.listen(PORT ,() =>{
+        console.log(`Server started at port ${PORT}`);
+    });
+
+}
+
+setupServer();
