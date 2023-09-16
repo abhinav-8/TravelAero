800d6f1046fb7458cbc2a9535af2ef0210713345
Abhinav Kumar <abhinavkumar.ak9@gmail.com>
2023-09-16 21:46:24 +0530

    added error files

---
+++ server/BOOKING_SERVICE/src/utils/errors/service-errors.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require('http-status-codes');
+
+class ServiceError extends Error {
+    constructor(
+        message = 'Something went wrong',
+        explanation = 'Service Layer Error',
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ) {
+        super();
+        this.name = 'ServiceError';
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+    }
+}
+
+module.exports = ServiceError;
